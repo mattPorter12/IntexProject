2f47d483c8667f3755c125b437406d2838f6773b
Matthew Scott Porter <mporte22@byu.edu>
2019-12-03 09:07:48 -0700

    Models on models

---
+++ Intex/Models/Compound.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Compound")]
+    public class Compound
+    {
+
+        [Key]
+        public int LTNumber { get; set; }
+        [Key]
+        public int SequenceCode { get; set; }
+
+        public int AssayId { get; set; }
+
+        public int PriorityNumber { get; set; }
+
+        [Required]
+        [DisplayName("Compound Name")]
+        public string CompName { get; set; }
+
+        [Required]
+        [DisplayName("Compound Quantity")]
+        public double CompQuantity { get; set; }
+
+        [Required]
+        [DisplayName("Arrival Date")]
+        [RegularExpression(@"^\d\d\\\d\d\\\d\d\d\d$", ErrorMessage = "Should be MM/DD/YYYY")]
+        public string ArrivalDate { get; set; }
+
+        [Required]
+        [DisplayName("Received By")]
+        public string ReceivedBy { get; set; }
+
+        [Required]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [DisplayName("Due Date")]
+        [RegularExpression(@"^\d\d\\\d\d\\\d\d\d\d$", ErrorMessage = "Should be MM/DD/YYYY")]
+        public string DueDate { get; set; }
+
+        [Required]
+        [DisplayName("Compound Appearance")]
+        public string CompAppearance { get; set; }
+
+        [Required]
+        [DisplayName("Compound Client Weight")]
+        public string CompClientWeight { get; set; }
+    }
+}
---
+++ Intex/Models/ConditionalTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Login")]
+    public class CondtionalTest
+    {
+        [Key]
+        public int TestID { get; set; }
+        
+        public int AssayID { get; set; }
+
+        [Required]
+        [DisplayName("Test Name")]
+        public string TestName { get; set; }
+
+
+        [Required]
+        [DisplayName("Test Protocol")]
+        public string TestProtocol { get; set; }
+
+
+    }
+}
