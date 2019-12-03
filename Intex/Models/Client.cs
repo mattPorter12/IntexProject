1dd0f99a84450923846f2cd845f42103633b9acb
Spencer Millett <millett.spencer@gmail.com>
2019-12-03 08:15:30 -0700

    Finished Client Class

--- Intex/Models/Client.cs
+++ Intex/Models/Client.cs
@@ -23,6 +23,35 @@ namespace Intex.Models
         public string PhysAddress1 { get; set; }
 
         [Required]
+        [DisplayName("Address 2")]
+        public string PhysAddress2 { get; set; }
 
+        [Required]
+        [DisplayName("City")]
+        public string PhysCity { get; set; }
+
+        [Required]
+        [DisplayName("State")]
+        public string PhysState { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d\d\d\d\d$", ErrorMessage = "Zip Code should be five digits")]
+        [DisplayName("Zip Code")]
+        public string PhysZipCode { get; set; }
+
+        [Required]
+        [DisplayName("Person to Contact")]
+        public string PointOfContact { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\(\d\d\d\) \d\d\d-\d\d\d\d$", ErrorMessage = "Please display phone number in this format: (000) 000-0000")]
+        [DisplayName("Phone Number")]
+        public string PointPhoneNum { get; set; }
+
+        [DisplayName("Discount Rate")]
+        public decimal DiscountRate { get; set; }
+
+        [DisplayName("Account Balance")]
+        public decimal Balance { get; set; }
     }
 }
