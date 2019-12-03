d6ecf28922c67e6eacf75abb3707377f9fab9c73
Matthew Scott Porter <mporte22@byu.edu>
2019-12-03 09:27:23 -0700

    more models

---
+++ Intex/Models/CompoundStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("CompoundStatus")]
+    public class CompoundStatus
+    {
+
+        [Key]
+        [Required]
+        public int CompStatusId { get; set; }
+
+        [Required]
+        public string CompStatusDesc { get; set; }
+    }
+}
---
+++ Intex/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [DisplayName("Employee First Name")]
+        public string EmpFirstName { get; set; }
+
+        [Required]
+        [DisplayName("Employee Last Name")]
+        public string EmpLastName { get; set; }
+
+        [Required]
+        [DisplayName("Employee Wage")]
+        public double HourlyWage { get; set; }
+    }
+}
