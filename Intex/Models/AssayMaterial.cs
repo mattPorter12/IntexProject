d80de3a95d61ab6f1fa281df20ecf01be855b107
Matthew Scott Porter <mporte22@byu.edu>
2019-12-03 08:37:40 -0700

    Assay material, material, test material

---
+++ Intex/Models/AssayMaterial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("AssayMaterial")]
+    public class AssayMaterial
+    {
+
+        [Key]
+        public int AssayID { get; set; }
+
+        [Key]
+        public int MaterialId { get; set; }
+    }
+}
---
+++ Intex/Models/Material.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Material")]
+    public class Material
+    {
+
+        [Key]
+        public int MaterialID { get; set; }
+
+        [Required]
+        [Display(Name = "Material Name")]
+        public string MatName { get; set; }
+
+        [Required]
+        [Display(Name = "Material Cost")]
+        public Double MatCost { get; set; }
+
+    }
+}
