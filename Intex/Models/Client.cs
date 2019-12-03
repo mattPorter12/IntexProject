834329ea0137c31dd6af84b87e06880c35a82da9
Spencer Millett <millett.spencer@gmail.com>
2019-12-03 07:50:39 -0700

    did stuff in the client model

--- Intex/Models/Client.cs
+++ Intex/Models/Client.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Intex.Models
-{
+{   
+    [Table("Client")]
     public class Client
     {
+        
+        [Key]
+        public int ClientID { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public string ClientName { get; set; }
+
+        [Required]
+        [Display(Name = "Address 1")]
+        public string PhysAddress1 { get; set; }
+
+        [Required]
+
     }
 }
