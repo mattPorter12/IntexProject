64d9622c42bbe0af51c3ff61d3c89764f7b6eee5
Spencer Millett <millett.spencer@gmail.com>
2019-12-03 08:21:54 -0700

    Login Class done

---
+++ Intex/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Login")]
+    public class Login
+    {
+        [Key]
+        [Required]
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+        public string ClientID { get; set; }
+    }
+}
