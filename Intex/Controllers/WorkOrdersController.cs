12a50983655311bbefb2e9aa8691846b5cf2a69c
Spencer Millett <millett.spencer@gmail.com>
2019-12-03 19:09:06 -0700

    Syncing

---
+++ Intex/Controllers/WorkOrdersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Intex.DAL;
+using Intex.Models;
+
+namespace Intex.Controllers
+{
+    public class WorkOrdersController : Controller
+    {
+        private NorthwestContext db = new NorthwestContext();
+
+        // GET: WorkOrders
+        public ActionResult Index()
+        {
+            var list = from p in db.OrderStatus
+                       orderby p.OrderStatusID
+                       select p;
+
+            ViewBag.OrderStatus = list;
+            return View(db.WorkOrder.ToList());
+        }
+
+        // GET: WorkOrders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WorkOrder workOrders = db.WorkOrder.Find(id);
+            if (workOrders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workOrders);
+        }
+
+        // GET: WorkOrders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WorkOrders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WorkOrderNum,ClientID,OrderDate,OrderStatusID")] WorkOrder workOrders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WorkOrder.Add(workOrders);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(workOrders);
+        }
+
+        // GET: WorkOrders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WorkOrder workOrders = db.WorkOrder.Find(id);
+            if (workOrders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workOrders);
+        }
+
+        // POST: WorkOrders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WorkOrderNum,ClientID,OrderDate,OrderStatusID")] WorkOrder workOrders)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(workOrders).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(workOrders);
+        }
+
+        // GET: WorkOrders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WorkOrder workOrders = db.WorkOrder.Find(id);
+            if (workOrders == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workOrders);
+        }
+
+        // POST: WorkOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WorkOrder workOrders = db.WorkOrder.Find(id);
+            db.WorkOrder.Remove(workOrders);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
