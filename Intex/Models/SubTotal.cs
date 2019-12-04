97d3596e499d8f67a9cd7017f394417ba71c9075
Paige Harper <paige.e.harper@gmail.com>
2019-12-04 13:12:48 -0700

    sync stuff

---
+++ Intex/Models/SubTotal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    public class SubTotal
+    {
+        [Key]
+        public int InvoiceID { get; set; }
+
+        [Required]
+        [DisplayName("Total Cost")]
+        public decimal TotalMatCost { get; set; }
+
+        [DisplayName("Client ID Number")]
+        public int ClientID { get; set; }
+
+        [Required]
+        [DisplayName("Due Date")]
+        [RegularExpression(@"^\d\d\\\d\d\\\d\d\d\d$", ErrorMessage = "Should be MM/DD/YYYY")]
+        public string DueDate { get; set; }
+
+        [Required]
+
+        [DisplayName("Early Date")]
+        [RegularExpression(@"^\d\d\\\d\d\\\d\d\d\d$", ErrorMessage = "Should be MM/DD/YYYY")]
+        public string EarlyDate { get; set; }
+
+        [Required]
+        [DisplayName("Early Discount Price")]
+        public decimal EarlyDiscount { get; set; }
+
+        [Required]
+        [DisplayName("SubTotal")]
+        public decimal SubTotal { get; set; }
+    }
+}
