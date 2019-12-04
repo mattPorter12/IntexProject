807ed2090e75232e03405057ff4985d7a3be726d
Matthew Scott Porter <mporte22@byu.edu>
2019-12-04 09:30:57 -0700

    Navbar, authorized controller

---
+++ Intex/Controllers/AuthorizedController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intex.Controllers
+{
+    [Authorize]
+    public class AuthorizedController : Controller
+    {
+        // GET: Authorized
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
